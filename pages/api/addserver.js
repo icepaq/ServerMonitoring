e135203dcdc7a9c43c37ebe6f2a60f039c0b7234
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-06-03 15:15:31 -0400

    Changed hi to result

--- pages/api/addserver.js
+++ pages/api/addserver.js
@@ -1,5 +1,5 @@
 const Main = require('./databaseactions/addserver.js')
 
 export default async (req, res) => {
-    res.status(200).json({hi: await Main.main(req.query.serverip, req.query.servername, req.query.email)});
+    res.status(200).json({result: await Main.main(req.query.serverip, req.query.servername, req.query.email)});
 }
