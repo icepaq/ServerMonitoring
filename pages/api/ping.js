f82e7b498b951d74918369f0693077842fed40f6
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-06-07 12:46:12 -0400

    Added /ping

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -1,10 +1,25 @@
-import Ping from 'ping';
+import { Cursor } from 'mongodb';
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://my_username:my_password@cluster0.dgxwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 export default async (req, res) => {
-    
-    var host ='google.com';
-    let r = await Ping.promise.probe(host);
-    console.log(r);
 
-    res.status(200).json({latency: r.avg})
+    const server = req.query.server;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });    
+    await client.connect();
+
+    const collection = client.db('serverpanel').collection('latency');
+    const cursor = collection.find({server: server}).sort({time: 1});
+
+    let results = [];
+
+    await cursor.forEach((result) => {
+        results.push({
+            latency: result.latency,
+            time: result.time
+        });
+    });
+
+    res.status(200).json({results: results});
 }
