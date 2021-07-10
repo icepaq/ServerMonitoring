46968d8dad34fdf77634062f85ed3d960c033575
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-07-09 20:40:50 -0400

    Update Keys.js

--- pages/keys/Keys.js
+++ pages/keys/Keys.js
@@ -2,7 +2,7 @@ import Styles from "../../styles/Home.module.css";
 import Greeting from "../GeneralComponents/greeting.js";
 import Box from "./box/Box.js";
 
-export default function ServerPage() {
+export default function keys() {
     return (
         <div className={Styles.overviewComponent}>
             <Greeting />
