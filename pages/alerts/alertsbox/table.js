ed577403d6c5d62f343bb077a3c47ab1017e3075
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-07-13 15:52:46 -0400

    alerts page

--- pages/alerts/alertsbox/table.js
+++ pages/alerts/alertsbox/table.js
@@ -1,6 +1,45 @@
 import Styles from "../../../styles/Alerts.module.css";
+import Cookies from "universal-cookie";
+import React, { useState } from "react";
 
 export default function table() {
+    const cookie = new Cookies();
+    const key = cookie.get("logincookie");
+    const email = cookie.get("logincookieemail");
+
+    const [data, setData] = useState();
+
+    fetch(
+        "http://localhost:3000/api/alertsmessages?email=" +
+            email +
+            "&key=" +
+            key
+    )
+        .then((res) => res.json())
+        .then((r) => {
+            let temp = [];
+            for (let i = 0; i < r.results.length; i++) {
+                let jsx = (
+                    <tr>
+                        <td className={Styles.tableData}>
+                            {r.results[i].name}
+                        </td>
+                        <td className={Styles.tableData}>
+                            {r.results[i].serverip}
+                        </td>
+                        <td className={Styles.tableData}>
+                            {r.results[i].message}
+                        </td>
+                        <td className={Styles.tableData}>
+                            {r.results[i].date}
+                        </td>
+                    </tr>
+                );
+                temp.push(jsx);
+            }
+            setData(temp);
+        });
+
     let tableData = (
         <tr>
             <td className={Styles.tableData}>Sample</td>
@@ -15,7 +54,7 @@ export default function table() {
             <th className={Styles.IPHeader}>IP Address</th>
             <th className={Styles.messageHeader}>Message</th>
             <th className={Styles.timeHeader}>Time</th>
-            {tableData}
+            {data}
         </table>
     );
 }
---
+++ pages/api/alertsmessages.js
@@ -0,0 +1,58 @@
+export default async (req, res) => {
+    let messages = [];
+    let servers = [];
+
+    const email = req.query.email;
+    const key = req.query.key;
+
+    // Get Servers
+    await fetch(
+        "http://localhost:3000/api/getServers?email=" + email + "&key=" + key
+    )
+        .then((res) => res.json())
+        .then((r) => {
+            for (let i = 0; i < r.result.length; i++) {
+                servers.push(r.result[i]);
+            }
+        });
+
+    // Generate Alert Messages
+    let d = new Date();
+    for (let i = 0; i < servers.length; i++) {
+        await fetch(
+            "http://localhost:3000/api/alerts?server=" +
+                servers[i].serverip +
+                "&key=" +
+                key
+        )
+            .then((res) => res.json())
+            .then((r) => {
+                if (r.results.down) {
+                    messages.push({
+                        name: servers[i].name,
+                        serverip: servers[i].serverip,
+                        message: "Server not responding",
+                        date: d.toLocaleString(),
+                    });
+                }
+                if (r.results.highlatency) {
+                    messages.push({
+                        name: servers[i].name,
+                        serverip: servers[i].serverip,
+                        message: "High latency",
+                        date: d.toLocaleString(),
+                    });
+                }
+                if (r.results.loss) {
+                    messages.push({
+                        name: servers[i].name,
+                        serverip: servers[i].serverip,
+                        message: "High packetloss",
+                        date: d.toLocaleString(),
+                    });
+                }
+            });
+    }
+
+    res.status(200).json({ results: messages });
+};
