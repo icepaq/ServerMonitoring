e42bf170a46047baf8b282f51c34b3892211f6f6
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-07-12 22:06:26 -0400

    Alerts counter and servers down

--- pages/alerts/alertsbox/InformationRow/InformationRow.js
+++ pages/alerts/alertsbox/InformationRow/InformationRow.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import Styles from "../../../../styles/Alerts.module.css";
 import Information from "./information.js";
+import Cookies from "universal-cookie";
 
 export default function InformationRow() {
+    const [Alerts, setAlerts] = useState("");
+    const [Servers, setServers] = useState("");
+    const [ServersDown, setServersDown] = useState("");
+
+    const cookie = new Cookies();
+    const key = cookie.get("logincookie");
+
+    fetch("http://localhost:3000/api/OverviewAPI?key=" + key)
+        .then((res) => res.json())
+        .then((r) => {
+            setAlerts(r.alerts);
+            setServers(r.servers);
+            setServersDown(r.down);
+        });
+
     return (
-        <>
-            <div className={Styles.informationRow}>
-                <Information number="1" title="Alert" />
-                <Information number="5" title="Server Down" />
-                <Information number="6" title="High Latency" />
-                <Information number="6" title="High Packetloss" />
-            </div>
-        </>
+        <div className={Styles.informationRow}>
+            <Information number={Alerts} title="Alert" />
+            <Information number={ServersDown} title="Server Down" />
+            <Information number="6" title="High Latency" />
+            <Information number="6" title="High Packetloss" />
+        </div>
     );
 }
