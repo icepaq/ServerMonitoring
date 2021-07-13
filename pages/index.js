0133e2cb79d821392c25d6d674aca24dc3b0439b
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-07-13 12:26:15 -0400

    redirect

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function index() {
-    return(
-        <h1 className={styles.hi}>Hi</h1>
-    )
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("/Login");
+    });
+
+    return <h1 className={styles.hi}>Hi</h1>;
 }
