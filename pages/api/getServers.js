f75cbb2e6f0b52389effa43476ec963bbb103626
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-07-16 18:01:30 -0400

    secure /getServers

--- pages/api/getServers.js
+++ pages/api/getServers.js
@@ -1,5 +1,18 @@
 import * as GetServers from "../api/databaseactions/getServers";
+const checkToken = require("./authenticator/modules/checkToken.js");
+
 
 export default async (req, res) => {
+
+
+    const check = new checkToken();
+    const key_email = await check.run(req.query.key);
+
+    if(key_email.email != req.query.email) {
+        console.log('GetServers authentication error');
+        res.status(200).json({ result: "AUTHENTICATION ERROR" });
+        return;
+    }
+
     res.status(200).json({ result: await GetServers.check(req.query.email) });
 };
