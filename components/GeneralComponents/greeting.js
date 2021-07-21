090a20073f1633c8102822be7d9e7bb4cecd0c59
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-07-21 14:37:56 -0400

    dynamic greeting

--- components/GeneralComponents/greeting.js
+++ components/GeneralComponents/greeting.js
@@ -1,5 +1,26 @@
 import Styles from "../../styles/Home.module.css";
+import Cookies from "universal-cookie";
+import { useState } from "react";
+
 
 export default function OverviewComponent() {
-    return <div className={Styles.greeting}>Hi, Anton!</div>;
+
+    const cookie = new Cookies();
+    const email = cookie.get("logincookieemail");
+    const key = cookie.get("logincookie");
+
+    const [name, setName] = useState('');
+
+    fetch("http://localhost/api/getUserConfig?email=" + email + "&key=" + key)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setName(data.results.name);
+        })
+        .catch(error => {
+            
+        });
+
+    return <div className={Styles.greeting}>Hi, {name}</div>;
+    
 }
